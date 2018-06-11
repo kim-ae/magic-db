a91063e0620b963035fe25b67210857d2ce803b9
Kim Aragon Escobar <kim.ae09@gmail.com>
2018-06-10 22:50:51 -0300

    Initializing project.

---
+++ src/main/java/br/com/kimae/MargicDbApplication.java
@@ -0,0 +1,12 @@
+package br.com.kimae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MargicDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MargicDbApplication.class, args);
+	}
+}
